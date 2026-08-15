613a316dda287efa4ea810ade6b92ce95295f256
Pierre <piercel.coder2022@gmail.com>
2026-08-15 23:02:09 +0200

    Add the modified team model to the project

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,14 @@
 class Team < ApplicationRecord
   belongs_to :organization
+
+  has_many :team_memberships
+  has_many :users, through: :team_memberships
+
+  has_many :jobs
+
+  validates :code,
+            presence: true,
+            uniqueness: { scope: :organization_id }
+
+  validates :name, presence: true
 end
