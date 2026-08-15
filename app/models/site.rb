c05266fd81009d9f4fc9e9d346c15883abf0081a
Pierre <piercel.coder2022@gmail.com>
2026-08-15 23:01:46 +0200

    Add the modified site model to the project

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,4 +1,10 @@
 class Site < ApplicationRecord
   belongs_to :organization
   belongs_to :customer
+
+  has_many :quotes
+  has_many :jobs
+  has_many :invoices
+
+  validates :name, presence: true
 end
