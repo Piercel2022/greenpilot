84d79d7d6ab3b04f9c3ca95dd9984d68d010bb6d
Pierre <piercel.coder2022@gmail.com>
2026-08-15 23:01:02 +0200

    Add the modified organization model to the project

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,26 @@
 class Organization < ApplicationRecord
+  has_many :users
+
+  has_many :customers
+  has_many :sites
+
+  has_many :service_categories
+  has_many :service_items
+
+  has_many :quotes
+  has_many :quote_items, through: :quotes
+
+  has_many :teams
+  has_many :team_memberships
+
+  has_many :vehicles
+  has_many :equipment
+
+  has_many :jobs
+  has_many :job_assignments
+  has_many :job_time_entries
+  has_many :job_reports
+
+  has_many :invoices
+  has_many :invoice_items, through: :invoices
 end
