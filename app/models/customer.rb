d58d2ce21d35d282c761f7cf0daae01aafa357b5
Pierre <piercel.coder2022@gmail.com>
2026-08-15 23:00:18 +0200

    Add the modified customer model to the project

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,13 @@
 class Customer < ApplicationRecord
   belongs_to :organization
+
+  has_many :sites
+  has_many :quotes
+  has_many :jobs
+  has_many :invoices
+
+  enum :customer_type, {
+    individual: "individual",
+    company: "company"
+  }, validate: true
 end
