a1a7b9cfdd28ebdad3c7403519b2a8efe740c1c0
Pierre <piercel.coder2022@gmail.com>
2026-08-15 23:02:32 +0200

    Add the modified user model to the project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   belongs_to :organization
+
+  has_many :team_memberships
+  has_many :teams, through: :team_memberships
+
+  has_many :job_assignments
+  has_many :jobs, through: :job_assignments
+
+  has_many :job_time_entries
 end
