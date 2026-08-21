770f812e7a7deefccf5c10a613ed805c747efb79
Pierre <piercel.coder2022@gmail.com>
2026-08-21 22:00:17 +0200

    Add the user model into the project

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,21 @@ class User < ApplicationRecord
   belongs_to :organization
 
   has_many :team_memberships
+
   has_many :teams, through: :team_memberships
 
   has_many :job_assignments
+
   has_many :jobs, through: :job_assignments
 
   has_many :job_time_entries
+
+  enum :role, {
+    member: "member",
+    field_worker: "field_worker",
+    accountant: "accountant",
+    manager: "manager",
+    admin: "admin",
+    owner: "owner"
+  }
 end
