d0ebc83561df28f11d5a92b5184cad7dd029a735
Pierre <piercel.coder2022@gmail.com>
2026-08-22 18:40:20 +0200

    Update organization fixture test

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class OrganizationTest < ActiveSupport::TestCase
   test "fixtures are loaded" do
     assert_equal 2, Organization.count
-    assert_equal 7, User.count
+    assert_equal 8, User.count
   end
 end
