0ebba82e1c79d50b8cb04d0cef112d58f507b59b
Pierre <piercel.coder2022@gmail.com>
2026-08-22 18:49:24 +0200

    Update the test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,18 +4,5 @@ require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
-  fixtures :organizations, 
-           :users, 
-           :customers, 
-           :sites, 
-           :teams, 
-           :team_memberships, 
-           :vehicles, 
-           :equipment, 
-           :service_categories,
-           :service_items,
-           :quotes,
-           :quote_items,
-           :jobs,
-           :job_assignments
+  fixtures :organizations, :users, :customers, :sites, :teams, :team_memberships, :vehicles, :equipment, :service_categories, :service_items, :quotes,:quote_items,:jobs,:job_assignments,:invoices, :invoice_items
 end
