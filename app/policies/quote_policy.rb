6585c4ef5180bfec5e8edee2d409800ceef4dbb7
Pierre <piercel.coder2022@gmail.com>
2026-08-22 11:19:19 +0200

    Update quote policy

---
+++ app/policies/quote_policy.rb
@@ -0,0 +1,54 @@
+class QuotePolicy < ApplicationPolicy
+  def index?
+    authenticated?
+  end
+
+  def show?
+    same_organization?
+  end
+
+  def create?
+    same_organization? && management?
+  end
+
+  def update?
+    same_organization? && management?
+  end
+
+  def destroy?
+    same_organization? && owner_or_admin?
+  end
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      scope
+        .joins(:customer, :site)
+        .where(
+          quotes: {
+            organization_id: user.organization_id
+          },
+          customers: {
+            organization_id: user.organization_id
+          },
+          sites: {
+            organization_id: user.organization_id
+          }
+        )
+    end
+  end
+
+  private
+
+  def same_organization?
+    return false unless authenticated?
+    return false unless record.organization
+    return false unless record.customer
+    return false unless record.site
+
+    return false unless record.organization_id == user.organization_id
+    return false unless record.customer.organization_id == user.organization_id
+    return false unless record.site.organization_id == user.organization_id
+
+    true
+  end
+end
