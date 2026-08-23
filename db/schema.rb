d8c66f856b36c3db6d2d56b1da093c63ef21aa7d
Pierre <piercel.coder2022@gmail.com>
2026-08-23 07:53:13 +0200

    Update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_12_145750) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_22_231341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -435,6 +435,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_12_145750) do
     t.string "last_name", null: false
     t.datetime "last_sign_in_at"
     t.uuid "organization_id", null: false
+    t.string "password_digest"
     t.string "phone"
     t.string "role", default: "member", null: false
     t.datetime "updated_at", null: false
