3878db742bb5787ed112e3d33b79389b0242b6d0
Pierre <piercel.coder2022@gmail.com>
2026-08-23 07:44:25 +0200

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+
   belongs_to :organization
 
   has_many :team_memberships
-
   has_many :teams, through: :team_memberships
 
   has_many :job_assignments
-
   has_many :jobs, through: :job_assignments
 
   has_many :job_time_entries
