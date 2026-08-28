8f1bbeaeeced35e732f2a5b8e5f75da7f85b607e
Pierre <piercel.coder2022@gmail.com>
2026-08-28 02:41:15 +0200

    fix: enforce team membership organization integrity

--- app/models/team_membership.rb
+++ app/models/team_membership.rb
@@ -8,6 +8,33 @@ class TeamMembership < ApplicationRecord
   validates :user_id,
             uniqueness: { scope: :team_id }
 
+  validate :team_belongs_to_organization
+  validate :user_belongs_to_organization
+
   scope :active, -> { where(active: true) }
   scope :inactive, -> { where(active: false) }
+
+  private
+
+  def team_belongs_to_organization
+    return if organization.blank? || team.blank?
+
+    if team.organization_id != organization_id
+      errors.add(
+        :team,
+        "must belong to the same organization"
+      )
+    end
+  end
+
+  def user_belongs_to_organization
+    return if organization.blank? || user.blank?
+
+    if user.organization_id != organization_id
+      errors.add(
+        :user,
+        "must belong to the same organization"
+      )
+    end
+  end
 end
