e0256218e0718a0942288ab575a6da24b730a004
Pierre <piercel.coder2022@gmail.com>
2026-08-28 23:19:01 +0200

    fix: clean up job reports controller

--- app/controllers/api/v1/job_reports_controller.rb
+++ app/controllers/api/v1/job_reports_controller.rb
@@ -1,3 +1,4 @@
+
 module Api
   module V1
     class JobReportsController < BaseController
@@ -16,28 +17,21 @@ module Api
       end
 
       def create
-         job_report = current_user.organization.job_reports.new(
-        job_report_params
-       )
+        job_report = current_user.organization.job_reports.new(
+          job_report_params
+        )
 
-       if job_report.invalid?
-       return render json: {
-       error: "Unprocessable Entity",
-       messages: job_report.errors.full_messages
-       }, status: :unprocessable_entity
+        authorize job_report
+
+        if job_report.save
+          render json: job_report, status: :created
+        else
+          render json: {
+            error: "Unprocessable Entity",
+            messages: job_report.errors.full_messages
+          }, status: :unprocessable_entity
+        end
       end
-
-      authorize job_report
-
-     if job_report.save
-      render json: job_report, status: :created
-     else
-    render json: {
-      error: "Unprocessable Entity",
-      messages: job_report.errors.full_messages
-    }, status: :unprocessable_entity
-     end
-    end
 
       def update
         authorize @job_report
