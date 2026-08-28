8822479f313cc5bfeff4a846450682eceb78748c
Pierre <piercel.coder2022@gmail.com>
2026-08-28 02:10:47 +0200

    fix: validate job report organization

--- app/models/job_report.rb
+++ app/models/job_report.rb
@@ -2,6 +2,8 @@ class JobReport < ApplicationRecord
   belongs_to :organization
   belongs_to :job
 
+  validate :job_belongs_to_organization
+
   scope :generated, -> { where.not(generated_at: nil) }
   scope :not_generated, -> { where(generated_at: nil) }
 
@@ -10,4 +12,17 @@ class JobReport < ApplicationRecord
 
   scope :signed, -> { where.not(customer_signed_at: nil) }
   scope :unsigned, -> { where(customer_signed_at: nil) }
+
+  private
+
+  def job_belongs_to_organization
+    return if organization.blank? || job.blank?
+
+    unless job.organization_id == organization_id
+      errors.add(
+        :job,
+        "must belong to the same organization"
+      )
+    end
+  end
 end
