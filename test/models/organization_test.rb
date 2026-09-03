e038983097db552b2eead27e030bdc2da8b09a50
Pierre <piercel.coder2022@gmail.com>
2026-09-03 22:05:42 +0200

    Expand organization model tests

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class OrganizationTest < ActiveSupport::TestCase
   test "fixtures are loaded" do
-    assert_equal 2, Organization.count
-    assert_equal 8, User.count
+    assert_equal 3, Organization.count
+    assert_equal 10, User.count
   end
 end
