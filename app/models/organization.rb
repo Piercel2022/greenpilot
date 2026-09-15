c42e5878750f1461f607c9ad38a5c835495025c4
Pierre <piercel.coder2022@gmail.com>
2026-09-15 20:11:11 +0200

    feat: update organization model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,7 @@
+
 class Organization < ApplicationRecord
+
+  has_one :subscription, dependent: :destroy
   has_many :users
 
   has_many :customers
@@ -23,4 +26,33 @@ class Organization < ApplicationRecord
 
   has_many :invoices
   has_many :invoice_items, through: :invoices
+
+  before_validation :generate_slug, on: :create
+
+  validates :name, presence: true
+  validates :slug, presence: true, uniqueness: true
+
+  private
+
+  def generate_slug
+    return if name.blank?
+
+    base_slug = name.to_s.parameterize
+    self.slug = base_slug if slug.blank?
+
+    return unless Organization.exists?(slug: slug)
+
+    suffix = 2
+
+    loop do
+      candidate = "#{base_slug}-#{suffix}"
+
+      unless Organization.exists?(slug: candidate)
+        self.slug = candidate
+        break
+      end
+
+      suffix += 1
+    end
+  end
 end
