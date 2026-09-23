ba144bdf502bdb3ed9a3f865dd48536ad97cb5e2
Pierre <piercel.coder2022@gmail.com>
2026-09-23 23:58:12 +0200

    Update the seed of the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -128,7 +128,7 @@ ApplicationRecord.transaction do
       email: "manager@greenpilot-paysage.fr",
       first_name: "Thomas",
       last_name: "Dubois",
-      phone: "0640012033",
+      phone: "0647012033",
       role: "manager"
     },
     {
