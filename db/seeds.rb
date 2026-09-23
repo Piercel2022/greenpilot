043d4f35571f8e6351b207dac9f29351febee038
Pierre <piercel.coder2022@gmail.com>
2026-09-23 23:42:31 +0200

    Update the seed of the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -112,7 +112,7 @@ ApplicationRecord.transaction do
       email: "owner@greenpilot-paysage.fr",
       first_name: "Pierre",
       last_name: "Martin",
-      phone: "0610000001",
+      phone: "0612000001",
       role: "owner"
     },
     {
